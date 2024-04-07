376207a3c6a5961dbf53092cd80ad98af17e70a2
Simran Sah <hasnarmis.work719@gmail.com>
2024-04-07 23:45:48 +0530

    update

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import os
 from dotenv import load_dotenv
 from langchain_community.llms.openai import OpenAI
-from langchain.text_splitter import TextSplitter
-from langchain.chains import llm
 from langchain.prompts import PromptTemplate
 
 load_dotenv()
@@ -13,7 +11,7 @@ os.environ["OPENAI_API_KEY"] = os.getenv("OPENAI_API_KEY")
 def generate_brd(text):
     temperature = 0.5
     llm=OpenAI(temperature=temperature)
-    prompt = PromptTemplate(input_variables=['text'], template='You are a software engineer at a tech company. You are tasked with writing a business requirements document (BRD) for the following requirements: \n\n{text}')
+    prompt = PromptTemplate(input_variables=['text'], template='You are a software engineer at a tech companyv and an expert at writing BRD. You are tasked with writing a business requirements document (BRD) for the following requirements: \n\n{text}')
 
     res=prompt.format(text=text)
     result=llm(res,max_tokens=2000)
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from app import generate_brd
 
 if __name__ == "__main__":
-    text = "The system shall allow users to log in using their email and password."
+    text = "Face Recognition System"
     result = generate_brd(text)
     print(result)
 
